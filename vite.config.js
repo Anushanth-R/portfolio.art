91997f66ff7a529c6ccdd71b6d17d257a0b17f48
Anushanth R <anushanth-r@users.noreply.github.com>
2025-06-15 18:51:58 +0530

    Updated resume PDF and config

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/anushanth.art/',
+  base: '/portfolio.art/',
   plugins: [react(), tailwindcss()],
 })
